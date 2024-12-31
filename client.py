8f5aa2d16f981a36364f97c038849303d5964ee1
Bykova Ksenia <117099157+whoiskseny@users.noreply.github.com>
2024-12-31 14:56:13 +0300

    Лабораторная Простой эхо-сервер

--- client.py
+++ client.py
@@ -1,16 +1,48 @@
+#ДПИ22-1 Быкова Ксения Дмитриевна
+
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+def my_client():
+    host = input("Введите адрес сервера: ").strip() or "localhost"
+    
+    while True:
+        try:
+            port = int(input("Введите порт сервера: "))
+            if 1 <= port <= 65535:
+                break
+            else:
+                print("Ошибка: введите корректный номер порта.")
+        except ValueError:
+            print("Ошибка: введите число.")
+    
+    print(f"Попытка подключения к серверу {host}:{port}...")
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.connect((host, port))
+            print(f"Успешное подкключение к серверу {host}:{port}.")
 
-data = sock.recv(1024)
+            while True:
+                message = input("Введите данные для отправки. Чтобы выйти введите exit: ")
+                
+                if message.lower() == 'exit':
+                    sock.send(message.encode())
+                    break
+                
+                sock.send(message.encode())
+                print("Данные успешно отправлены.")
 
-sock.close()
+                data = sock.recv(1024).decode()
+                print(f"Приём данных от сервера: {data}")
 
-print(data.decode())
+        except ConnectionRefusedError:
+            print(f"Подключение к серверу {host}:{port} не установлено.")
+        except KeyboardInterrupt:
+            print("\nПринудительный разрыв подключения.")
+
+    print("Прекращение подключения к серверу...")
+    sock.close()
+    print("Подключение остановлено.")
+
+if __name__ == "__main__":
+    my_client()
